cab0dc9563e627de4470eddf649d6ddfd1f46ce9
alilinkin <sail94@mail.ru>
2022-09-11 21:11:57 +0500

    Максимальное число из 3

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
-﻿// Выбор большего числа из 2, за основу возьмем что пользователь вводит 2 целых числа
+﻿// Выбор максимального числа из 3, за основу возьмем что пользователь вводит 3 целых числа
 Console.Write ("Введите первое число: ");
 int number1 = Convert.ToInt32 (Console.ReadLine());
 
 Console.Write ("Введите второе число: ");
 int number2 = Convert.ToInt32 (Console.ReadLine());
 
-if (number1>number2)
-{
-     Console.Write(number1);
-}
-else
-{
-     Console.Write(number2);
-}
+Console.Write ("Введите третье число: ");
+int number3 = Convert.ToInt32 (Console.ReadLine());
+
+int max = number1;
+if (number2> number1)
+    max = number2;
+if (number3> max)
+    max = number3;
+
+Console.Write (max);
