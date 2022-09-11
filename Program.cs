4f44627c039aeedf401f22479c38dc492ccc9219
alilinkin <sail94@mail.ru>
2022-09-11 20:22:54 +0500

    Финальная версия программы

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Выбор большего числа из 2, за основу возьмем что пользователь вводит 2 целых числа
+Console.Write ("Введите первое число: ");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Введите второе число: ");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+
+if (number1>number2)
+{
+     Console.Write(number1);
+}
+
+else;
+{
+     Console.Write(number2);
+}
+
+// Программа не рабоет если первое число отрицательное и больше вторго отрицательного числа (-3,-9)
